b2ba0f81f453cfddd3235867f6bf996fcce2f7d2
Diego García <dagarciam1014@gmail.com>
2022-05-01 02:50:15 -0400

    Created Point and Size structs
    Should eventually be made as a more C# like wrapper around SDL's types
    Is it worth it?

---
+++ Point.cs
@@ -0,0 +1,13 @@
+﻿namespace SDL2.NET;
+
+public struct Point
+{
+    public int X { get; }
+    public int Y { get; }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
---
+++ Size.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2.NET;
+public struct Size
+{
+    public int Width { get; }
+    public int Height { get; }
+
+    public Size(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+}
