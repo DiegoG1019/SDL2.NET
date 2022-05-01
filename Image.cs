942559d559cb603bc6eb6f4ce2d97bf709e21035
Diego García <dagarciam1014@gmail.com>
2022-05-01 02:49:26 -0400

    Create Image.cs

---
+++ Image.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2.NET;
+public class Image : IDisposable
+{
+    internal readonly IntPtr _handle = IntPtr.Zero;
+
+    #region IDisposable
+
+    private bool disposedValue;
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            SDL.SDL_FreeSurface(_handle);
+            disposedValue = true;
+        }
+    }
+
+    ~Image()
+    {
+        Dispose(disposing: false);
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    private void ThrowIfDisposed()
+    {
+        if (disposedValue)
+            throw new ObjectDisposedException(nameof(Application));
+    }
+
+    #endregion
+}
