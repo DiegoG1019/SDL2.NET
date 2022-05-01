b22c6b164e23681905e1bf4cd0be306a2b0d5333
Diego García <dagarciam1014@gmail.com>
2022-04-30 21:44:45 -0400

    Create Renderer class
    To act as a wrapper for SDL renderer

---
+++ Renderer.cs
@@ -0,0 +1,50 @@
+﻿using SDL2.NET.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2.NET;
+public class Renderer : IDisposable
+{
+#error Not Implemented
+    internal readonly IntPtr _handle = IntPtr.Zero;
+    private readonly Window AttachedWindow;
+
+    public Renderer(Window window, int index) : this(window, index, SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED | SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC) { }
+
+    public Renderer(Window window, int index, SDL.SDL_RendererFlags flags)
+    {
+        _handle = SDL.SDL_CreateRenderer(window._handle, -1, flags);
+        if (_handle == IntPtr.Zero)
+            throw new SDLRendererCreationException(SDL.SDL_GetError());
+        AttachedWindow = window;
+    }
+
+    #region IDisposable
+
+    private bool disposedValue;
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            SDL.SDL_DestroyRenderer(_handle);
+            disposedValue = true;
+        }
+    }
+
+    ~Renderer()
+    {
+        Dispose(disposing: false);
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    #endregion
+}
