fe9c2b4358d191db5550a3ac7c599e8d974dab82
Diego García <dagarciam1014@gmail.com>
2022-05-01 22:45:05 -0400

    Added SDLRendererException

--- Exceptions/SDLException.cs
+++ Exceptions/SDLException.cs
@@ -117,3 +117,28 @@ public class SDLWindowException : SDLException
             throw new SDLWindowException(SDL.SDL_GetError());
     }
 }
+
+[Serializable]
+public class SDLRendererException : SDLException
+{
+    public SDLRendererException() { }
+    public SDLRendererException(string message) : base(message) { }
+    public SDLRendererException(string message, Exception inner) : base(message, inner) { }
+    protected SDLRendererException(
+      System.Runtime.Serialization.SerializationInfo info,
+      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ThrowIfLessThan(int value, int comparison)
+    {
+        if (value < comparison)
+            throw new SDLRendererException(SDL.SDL_GetError());
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ThrowIfEquals(int value, int comparison)
+    {
+        if (value == comparison)
+            throw new SDLRendererException(SDL.SDL_GetError());
+    }
+}
