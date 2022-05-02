c7133d3c2ed1fc0218e4839c7160e26a0722d835
Diego García <dagarciam1014@gmail.com>
2022-05-01 22:45:15 -0400

    Separated Surface and Image

--- Image.cs
+++ Image.cs
@@ -5,39 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SDL2.NET;
-public class Image : IDisposable
+public class Image : Surface
 {
-    internal readonly IntPtr _handle = IntPtr.Zero;
 
-    #region IDisposable
-
-    private bool disposedValue;
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (!disposedValue)
-        {
-            SDL.SDL_FreeSurface(_handle);
-            disposedValue = true;
-        }
-    }
-
-    ~Image()
-    {
-        Dispose(disposing: false);
-    }
-
-    public void Dispose()
-    {
-        Dispose(disposing: true);
-        GC.SuppressFinalize(this);
-    }
-
-    private void ThrowIfDisposed()
-    {
-        if (disposedValue)
-            throw new ObjectDisposedException(nameof(SDLApplication));
-    }
-
-    #endregion
 }
+
+#error Not Implemented
---
+++ Surface.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2.NET;
+public class Surface : IDisposable
+{
+    protected internal readonly IntPtr _handle = IntPtr.Zero;
+
+    #region IDisposable
+
+    private bool disposedValue;
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            SDL.SDL_FreeSurface(_handle);
+            disposedValue = true;
+        }
+    }
+
+    ~Surface()
+    {
+        Dispose(disposing: false);
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected void ThrowIfDisposed()
+    {
+        if (disposedValue)
+            throw new ObjectDisposedException(nameof(Surface));
+    }
+
+    #endregion
+}
+
+#error Not Implemented
