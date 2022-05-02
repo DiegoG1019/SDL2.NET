412161ad6a625049a634dff385acb0ee0c0b6e2c
Diego García <dagarciam1014@gmail.com>
2022-05-02 00:42:29 -0400

    Added SDL_Point conversion in Size

--- Size.cs
+++ Size.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static SDL2.SDL;
 
 namespace SDL2.NET;
 public struct Size
@@ -14,5 +15,17 @@ public struct Size
     {
         Width = width;
         Height = height;
+    }
+
+    public static implicit operator Size(SDL_Point point)
+        => new(point.x, point.y);
+
+    internal void ToSDLPoint(ref SDL_Point point)
+    {
+        point = new SDL_Point()
+        {
+            x = Width,
+            y = Height
+        };
     }
 }
