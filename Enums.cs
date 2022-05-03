2798f3b326ee352bf5d7167f757293ed61b75a41
Diego García <dagarciam1014@gmail.com>
2022-05-02 23:39:45 -0400

    Create Enums.cs

---
+++ Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2.NET;
+
+public enum FullscreenMode : uint
+{
+    Windowed = 0,
+    Fullscreen = SDL.SDL_WindowFlags.SDL_WINDOW_FULLSCREEN,
+    DesktopFullscreen = SDL.SDL_WindowFlags.SDL_WINDOW_FULLSCREEN_DESKTOP,
+}
