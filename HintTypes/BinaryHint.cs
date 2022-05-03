6e6057b5d8d0b3bf2301e91d4186189f2e9d9801
Diego García <dagarciam1014@gmail.com>
2022-05-03 19:37:48 -0400

    Added base SDL hint types

---
+++ HintTypes/BinaryHint.cs
@@ -0,0 +1,17 @@
+﻿namespace SDL2.NET.HintTypes;
+
+public class BinaryHint : Hint
+{
+    internal BinaryHint(string name) : base(name) { }
+
+    public void SetPriority(bool isEnabled, HintPriority priority) 
+        => Set(isEnabled ? "1" : "0", priority);
+
+    public bool IsEnabled
+    {
+        get => Get() == "1";
+        set => Set(value ? "1" : "0");
+    }
+}
+
+//public delegate void HintCallback();
---
+++ HintTypes/Hint.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SDL2.SDL;
+
+namespace SDL2.NET.HintTypes;
+public class Hint
+{
+    //private UserData? callbackUserData;
+    //private HintCallback? callback;
+
+    private readonly string HintName;
+
+    internal Hint(string name) 
+    {
+        HintName = name;
+        //SDL.SDL_AddHintCallback()
+    }
+
+    protected bool Set(string value)
+        => SDL_SetHint(HintName, value) is SDL_bool.SDL_TRUE;
+
+    protected bool Set(string value, HintPriority priority)
+        => SDL_SetHintWithPriority(HintName, value, priority.ToSDL()) is SDL_bool.SDL_TRUE;
+
+    protected string? Get()
+        => SDL_GetHint(HintName);
+
+    /// <summary>
+    /// Get the boolean value of a hint variable. <see cref="SDL_GetHintBoolean" href="https://wiki.libsdl.org/SDL_GetHintBoolean"/>
+    /// </summary>
+    /// <param name="def">The value to return if the hint does not exist</param>
+    /// <returns>The boolean value of a hint</returns>
+    public bool GetBool(bool def = true)
+        => SDL_GetHintBoolean(HintName, def ? SDL_bool.SDL_TRUE : SDL_bool.SDL_FALSE) is SDL_bool.SDL_TRUE;
+}
+
+//public delegate void HintCallback();
