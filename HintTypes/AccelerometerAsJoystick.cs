4a5687cc677b8c34ed336f3c7dd0b6fe751ce388
Diego García <dagarciam1014@gmail.com>
2022-05-03 19:37:58 -0400

    Create AccelerometerAsJoystick.cs

---
+++ HintTypes/AccelerometerAsJoystick.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Versioning;
+using static SDL2.SDL;
+
+namespace SDL2.NET.HintTypes;
+
+/// <summary>
+/// A hint that specifies whether the Android / iOS built-in accelerometer should be listed as a joystick device, rather than listing actual joysticks only. <see cref="SDL_HINT_ACCELEROMETER_AS_JOYSTICK" href="https://wiki.libsdl.org/SDL_HINT_ACCELEROMETER_AS_JOYSTICK"/>
+/// </summary>
+/// <remarks>
+/// By default SDL will list real joysticks along with the accelerometer as if it were a 3 axis joystick.
+/// </remarks>
+public sealed class AccelerometerAsJoystick : BinaryHint
+{
+    internal AccelerometerAsJoystick() : base(SDL_HINT_ACCELEROMETER_AS_JOYSTICK) { }
+}
+
+//public delegate void HintCallback();
