d3983c7166e9a978d7c14409c3ac04189f880497
Diego García <dagarciam1014@gmail.com>
2022-05-03 19:38:15 -0400

    Create EmscriptenAsyncify.cs

---
+++ HintTypes/EmscriptenAsyncify.cs
@@ -0,0 +1,14 @@
+﻿using static SDL2.SDL;
+
+namespace SDL2.NET.HintTypes;
+
+/// <summary>
+/// A hint that specifies if SDL should give back control to the browser automatically when running with asyncify. <see cref="SDL_HINT_EMSCRIPTEN_ASYNCIFY" href="https://wiki.libsdl.org/SDL_HINT_EMSCRIPTEN_ASYNCIFY"/>
+/// </summary>
+/// <remarks>
+/// This hint only applies to the Emscripten platform.
+/// </remarks>
+public sealed class EmscriptenAsyncify : BinaryHint
+{
+    internal EmscriptenAsyncify() : base(SDL_HINT_EMSCRIPTEN_ASYNCIFY) { }
+}
