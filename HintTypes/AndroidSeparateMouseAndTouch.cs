0d66bc74adbd17ad104649049baec03e55ec39d7
Diego García <dagarciam1014@gmail.com>
2022-05-03 19:38:04 -0400

    Create AndroidSeparateMouseAndTouch.cs

---
+++ HintTypes/AndroidSeparateMouseAndTouch.cs
@@ -0,0 +1,14 @@
+﻿using static SDL2.SDL;
+
+namespace SDL2.NET.HintTypes;
+
+/// <summary>
+/// A hint that specifies a variable to control whether mouse and touch events are to be treated together or separately. <see cref="SDL_HINT_ANDROID_SEPARATE_MOUSE_AND_TOUCH" href="https://wiki.libsdl.org/SDL_HINT_ANDROID_SEPARATE_MOUSE_AND_TOUCH"/>
+/// </summary>
+/// <remarks>
+/// The value of this hint is used at runtime, so it can be changed at any time. By default mouse events will be handled as touch events and touch will raise fake mouse events.
+/// </remarks>
+public sealed class AndroidSeparateMouseAndTouch : BinaryHint
+{
+    internal AndroidSeparateMouseAndTouch() : base(SDL_HINT_ANDROID_SEPARATE_MOUSE_AND_TOUCH) { }
+}
