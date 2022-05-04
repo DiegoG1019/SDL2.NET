bdba1468d76e0b45f41017c5c6d4b6338290ed5a
Diego García <dagarciam1014@gmail.com>
2022-05-04 00:30:45 -0400

    Update WindowRenderer.cs

--- WindowRenderer.cs
+++ WindowRenderer.cs
@@ -4,9 +4,9 @@ public class WindowRenderer : Renderer
 {
     public Window AttachedWindow { get; }
 
-    public WindowRenderer(Window window, int index) : this(window, index, SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED | SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC) { }
+    public WindowRenderer(Window window, int index = -1) : this(window, SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED | SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC, index) { }
 
-    public WindowRenderer(Window window, int index, SDL.SDL_RendererFlags flags) : base(SDL.SDL_CreateRenderer(window._handle, -1, flags))
+    public WindowRenderer(Window window, SDL.SDL_RendererFlags flags, int index = -1) : base(SDL.SDL_CreateRenderer(window._handle, index, flags))
     {
         AttachedWindow = window;
     }
