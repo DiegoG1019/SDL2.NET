5145e200488a83266250daa5f9396e0dcf1b4de9
Diego García <dagarciam1014@gmail.com>
2022-05-06 01:45:55 -0400

    Create FRectangle.cs

---
+++ FRectangle.cs
@@ -0,0 +1,33 @@
+﻿using static SDL2.SDL;
+
+namespace SDL2.NET;
+
+public struct FRectangle
+{
+    public float Width { get; }
+    public float Height { get; }
+    public float X { get; }
+    public float Y { get; }
+
+    public FRectangle(float width, float height, float x, float y)
+    {
+        Width = width;
+        Height = height;
+        X = x;
+        Y = y;
+    }
+
+    public static implicit operator FRectangle(SDL_FRect rect)
+        => new(rect.w, rect.h, rect.x, rect.y);
+
+    internal void ToSDLFRect(ref SDL_FRect rect)
+    {
+        rect = new SDL_FRect()
+        {
+            h = Height,
+            w = Width,
+            x = X,
+            y = Y
+        };
+    }
+}
