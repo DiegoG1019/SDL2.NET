b923c17ea33f9c215f7c420321a30471ae2017f7
Diego García <dagarciam1014@gmail.com>
2022-05-16 18:50:04 -0400

    Update Surface.cs

--- SDL2.NET/Surface.cs
+++ SDL2.NET/Surface.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using SDL2.Bindings;
+using SDL2.NET.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using static SDL2.Bindings.SDL;
 
 namespace SDL2.NET;
 
@@ -19,12 +23,38 @@ public class Surface : IDisposable
     /// Performs a fast surface copy to a destination surface.
     /// </summary>
     /// <remarks>You should call <see cref="Blit"/> unless you know exactly how SDL blitting works internally and how to use the other blit functions. The blit function should not be called on a locked <see cref="Surface"/>.</remarks>
-    /// <param name="destination"></param>
-    /// <param name="sourceRect"></param>
-    /// <param name="destinationRect"></param>
-    public void Blit(Surface destination, Rectangle? sourceRect, Rectangle? destinationRect)
+    /// <param name="destination">The blit target</param>
+    /// <param name="sourceRect">The <see cref="Rectangle"/> representing the area to be copied, or <see cref="null"/> to copy the entire surface</param>
+    /// <param name="destinationRect">The <see cref="Rectangle"/> representing the area to be copied into, or <see cref="null"/> to copy the entire surface. <see cref="Rectangle.Width"/> and <see cref="Rectangle.Height"/> are ignored, and only <see cref="Rectangle.X"/> and <see cref="Rectangle.Y"/> are taken into account</param>
+    public void Blit(Surface destination, Rectangle? sourceRect = null, Rectangle? destinationRect = null)
     {
-        
+        ThrowIfDisposed();
+        IntPtr srect = IntPtr.Zero;
+        IntPtr drect = IntPtr.Zero;
+
+        try
+        {
+            if (sourceRect is Rectangle src)
+            {
+                srect = Marshal.AllocHGlobal(Marshal.SizeOf(src));
+                Marshal.StructureToPtr(src, srect, false);
+            }
+
+            if (destinationRect is Rectangle dst)
+            {
+                drect = Marshal.AllocHGlobal(Marshal.SizeOf(dst));
+                Marshal.StructureToPtr(dst, drect, false);
+            }
+
+            SDLTextureException.ThrowIfLessThan(SDL_BlitSurface(_handle, srect, destination._handle, drect), 0);
+        }
+        finally
+        {
+            if (srect != IntPtr.Zero)
+                Marshal.FreeHGlobal(srect);
+            if (drect != IntPtr.Zero)
+                Marshal.FreeHGlobal(drect);
+        }
     }
 
     #region IDisposable
