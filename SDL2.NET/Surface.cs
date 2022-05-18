ecd85060968ec27eb508a02700832e824d1ba82b
Diego García <dagarciam1014@gmail.com>
2022-05-17 23:11:26 -0400

    Update Surface.cs

--- SDL2.NET/Surface.cs
+++ SDL2.NET/Surface.cs
@@ -19,7 +19,12 @@ public class Surface : IDisposable
 {
     protected internal readonly IntPtr _handle = IntPtr.Zero;
 
-    private Surface(IntPtr handle) => _handle = handle;
+    private Surface(IntPtr handle)
+    {
+        _handle = handle;
+        if (_handle == IntPtr.Zero)
+            throw new SDLSurfaceCreationException(SDL_GetError());
+    }
 
     /// <summary>
     /// Creates a new RGB surface
@@ -35,7 +40,7 @@ public class Surface : IDisposable
         : this(SDL_CreateRGBSurface(0, width, height, depth, rmask, gmask, bmask, amask)) { }
 
     /// <summary>
-    /// Creates a new RGB surface
+    /// Creates a new RGB surface. <see cref="SDL_CreateRGBSurfaceWithFormat" href="https://wiki.libsdl.org/SDL_CreateRGBSurfaceWithFormat"/>
     /// </summary>
     /// <param name="width">The width of the surface</param>
     /// <param name="height">The Height of the surface</param>
@@ -47,7 +52,12 @@ public class Surface : IDisposable
 #warning missing https://wiki.libsdl.org/SDL_CreateRGBSurfaceFrom
 
     /// <summary>
-    /// Copy an existing <see cref="Surface"/> to a new <see cref="Surface"/> of the specified format.
+    /// The format of the pixels stored in the surface
+    /// </summary>
+    public PixelFormatData PixelFormatData { get; }
+
+    /// <summary>
+    /// Copy an existing <see cref="Surface"/> to a new <see cref="Surface"/> of the specified format. <see cref="SDL_ConvertSurfaceFormat" href="https://wiki.libsdl.org/SDL_ConvertSurfaceFormat"/>
     /// </summary>
     /// <param name="format">the <see cref="PixelFormat"/> that the new surface is optimized for</param>
     /// <returns>Returns the new <see cref="Surface"/> structure that is created</returns>
