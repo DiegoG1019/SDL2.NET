6f3656425463133dc8848ae33fa1538be9451de4
Diego García <dagarciam1014@gmail.com>
2022-05-18 19:04:34 -0400

    Update UserData.cs

--- SDL2.NET/UserData.cs
+++ SDL2.NET/UserData.cs
@@ -8,52 +8,42 @@ using System.Threading.Tasks;
 namespace SDL2.NET;
 public abstract class UserData
 {
-    internal UserData() { }
-    public abstract Type GetDataType();
-    public abstract bool Match(Type type);
-    public abstract bool Match<TMatch>();
-    public abstract bool Match(Type type, [MaybeNullWhen(false)] out object? data);
-    public abstract bool Match<TMatch>([MaybeNullWhen(false)] out TMatch data);
+    internal UserData(bool isStruct) => IsValueType = isStruct;
+
+    /// <summary>
+    /// Gets the <see cref="Type"/> of the held <see cref="Data"/>
+    /// </summary>
+    public abstract Type DataType { get; }
+
+    /// <summary>
+    /// Gets an abstracted (up-casted) form of the held data. This involves a boxing operationg if the held data is a <see cref="ValueType"/> (<see cref="struct"/> or <see cref="ValueTuple"/>). See <see cref="IsValueType"/> if you want to avoid boxing
+    public abstract object? Value { get; }
+    
+    /// <summary>
+    /// Whether the held data is a struct or not
+    /// </summary>
+    public bool IsValueType { get; }
 }
 
 public sealed class UserData<T> : UserData
 {
     private T _data;
+
+    /// <summary>
+    /// Provides a <see cref="ref"/> to the held data; for instances where you'd want to modify the fields of a <see cref="ValueType"/>
+    /// </summary>
     public ref T RefData => ref _data;
+
+    /// <summary>
+    /// The data held by this instance
+    /// </summary>
     public T Data
     {
         get => _data;
         set => _data = value;
     }
 
-    public UserData(T data) => _data = data;
-
-    public override Type GetDataType() => typeof(T);
-    public override bool Match(Type type, [MaybeNullWhen(false)] out object? data)
-    {
-        if (typeof(T) == type)
-        {
-            data = _data;
-            return true;
-        }
-        data = null;
-        return false;
-    }
-
-    public override bool Match(Type type)
-        => typeof(T) == type;
-
-    public override bool Match<TMatch>()
-        => _data is TMatch;
-
-    public override bool Match<TMatch>([MaybeNullWhen(false)] out TMatch data)
-    {
-        if (_data is TMatch d)
-        {
-            data = d;
-            return true;
-        }
-        data = default;
-        return false;
-    }
+    public UserData(T data) : base(typeof(T).IsValueType) => _data = data;
+    public override object? Value => _data;
+    public override Type DataType => typeof(T);
 }
