41766ad8785a5549a2f0ff48cb5cfbe95b7cc244
Diego García <dagarciam1014@gmail.com>
2022-05-19 01:05:37 -0400

    Started work with SDL2.TTF support

---
+++ SDL2.NET/Font/Enums.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SDL2.Bindings.SDL_ttf;
+
+namespace SDL2.NET.Font;
+
+public enum UnicodeType
+{
+    Native = UNICODE_BOM_NATIVE,
+    SWAPPED = UNICODE_BOM_SWAPPED
+}
+
+[Flags]
+public enum TTFStyle
+{
+    Normal = TTF_STYLE_NORMAL,
+    Bold = TTF_STYLE_BOLD,
+    Italic = TTF_STYLE_ITALIC,
+    Underline = TTF_STYLE_UNDERLINE,
+    Strikethrough = TTF_STYLE_STRIKETHROUGH,
+}
+
+public enum TTFHinting
+{
+    Normal = TTF_HINTING_NORMAL,
+    Light = TTF_HINTING_LIGHT,
+    Mono = TTF_HINTING_MONO,
+    None = TTF_HINTING_NONE,
+    LightSubpixel = TTF_HINTING_LIGHT_SUBPIXEL,
+}
+
---
+++ SDL2.NET/Font/Font.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SDL2.Bindings.SDL_ttf;
+
+namespace SDL2.NET.Font;
+public static class Font
+{
+
+    static Font()
+    {
+        var v = TTF_LinkedVersion();
+        SDLTTFVersion = new(v.major, v.minor, v.patch);
+    }
+
+    /// <summary>
+    /// The version of SDL_image. <see cref="TTF_LinkedVersion"/>
+    /// </summary>
+    public static Version SDLTTFVersion { get; }
+}
