b155aae0fbc04b6e7b006e063a806975f34b4a1a
Diego García <dagarciam1014@gmail.com>
2022-05-19 11:23:25 -0400

    Create TextMeasurement.cs

---
+++ SDL2.NET/Font/TextMeasurement.cs
@@ -0,0 +1,13 @@
+﻿namespace SDL2.NET.Font;
+
+public struct TextMeasurement
+{
+    public int Extent { get; }
+    public int Count { get; }
+
+    public TextMeasurement(int extent, int count)
+    {
+        Extent = extent;
+        Count = count;
+    }
+}
