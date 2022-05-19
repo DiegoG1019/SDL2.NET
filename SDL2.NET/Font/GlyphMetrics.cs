79e26f1c8ae81f66d3f1553fc3b94a9fb618244a
Diego García <dagarciam1014@gmail.com>
2022-05-19 11:23:20 -0400

    Moved GlyphMetrics to its own file

---
+++ SDL2.NET/Font/GlyphMetrics.cs
@@ -0,0 +1,19 @@
+﻿namespace SDL2.NET.Font;
+
+public struct GlyphMetrics
+{
+    public int MinimumX { get; }
+    public int MinimumY { get; }
+    public int MaximumX { get; }
+    public int MaximumY { get; }
+    public int Advance { get; }
+
+    public GlyphMetrics(int minx, int miny, int maxx, int maxy, int advance)
+    {
+        MinimumX = minx;
+        MinimumY = miny;
+        MaximumX = maxx;
+        MaximumY = maxy;
+        Advance = advance;
+    }
+}
--- SDL2.NET/Font/TTFont.cs
+++ SDL2.NET/Font/TTFont.cs
@@ -224,21 +224,3 @@ public class TTFont : IDisposable
 
     #endregion
 }
-
-public struct GlyphMetrics
-{
-    public int MinimumX { get; }
-    public int MinimumY { get; }
-    public int MaximumX { get; }
-    public int MaximumY { get; }
-    public int Advance { get; }
-
-    public GlyphMetrics(int minx, int miny, int maxx, int maxy, int advance)
-    {
-        MinimumX = minx;
-        MinimumY = miny;
-        MaximumX = maxx;
-        MaximumY = maxy;
-        Advance = advance;
-    }
-}
