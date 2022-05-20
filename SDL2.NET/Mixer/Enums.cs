c7c7600d488e4e9e425db212ddd32d38b664e497
Diego García <dagarciam1014@gmail.com>
2022-05-20 11:58:55 -0400

    Update Enums.cs

--- SDL2.NET/Mixer/Enums.cs
+++ SDL2.NET/Mixer/Enums.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static SDL2.Bindings.SDL_mixer;
 
 namespace SDL2.NET.Mixer;
 
@@ -15,4 +16,11 @@ public enum MixerInitFlags
     OGG = 0x00000010,
     MID = 0x00000020,
     OPUS = 0x00000040
+}
+
+public enum AudioFadeStatus
+{
+    NotFading = Mix_Fading.MIX_NO_FADING,
+    FadingOut = Mix_Fading.MIX_FADING_OUT,
+    FadingIn = Mix_Fading.MIX_FADING_IN
 }
