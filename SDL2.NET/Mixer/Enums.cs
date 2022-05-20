265bc4a3739335c35dd07479280fe53aaa8e2524
Diego García <dagarciam1014@gmail.com>
2022-05-19 23:33:40 -0400

    Create Enums.cs

---
+++ SDL2.NET/Mixer/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2.NET.Mixer;
+
+[Flags]
+public enum MixerInitFlags
+{
+    FLAC = 0x00000001,
+    MOD = 0x00000002,
+    MP3 = 0x00000008,
+    OGG = 0x00000010,
+    MID = 0x00000020,
+    OPUS = 0x00000040
+}
