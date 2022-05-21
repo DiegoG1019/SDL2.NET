935fb09b066ce96d80600b8ae31c39ddd8b9337f
Diego García <dagarciam1014@gmail.com>
2022-05-21 01:34:13 -0400

    Create DisableThreadNaming.cs

---
+++ SDL2.NET/HintTypes/DisableThreadNaming.cs
@@ -0,0 +1,14 @@
+﻿using SDL2.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2.NET.HintTypes;
+public sealed class DisableThreadNaming : BinaryHint
+{
+    internal DisableThreadNaming() : base(SDL.SDL_HINT_WINDOWS_DISABLE_THREAD_NAMING)
+    {
+    }
+}
