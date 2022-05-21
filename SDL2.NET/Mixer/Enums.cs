f1ee4adead4182990722564ba5d5e52109cb2cd4
Diego García <dagarciam1014@gmail.com>
2022-05-21 00:01:51 -0400

    Added MusicType enum

--- SDL2.NET/Mixer/Enums.cs
+++ SDL2.NET/Mixer/Enums.cs
@@ -24,3 +24,58 @@ public enum AudioFadeStatus
     FadingOut = Mix_Fading.MIX_FADING_OUT,
     FadingIn = Mix_Fading.MIX_FADING_IN
 }
+
+public enum MusicType
+{
+    /// <summary>
+    /// No music type
+    /// </summary>
+    None = Mix_MusicType.MUS_NONE,
+
+    /// <summary>
+    /// Command Based Music
+    /// </summary>
+    CommandBased = Mix_MusicType.MUS_CMD,
+
+    /// <summary>
+    /// Waveform Audio File Format: <see href="https://en.wikipedia.org/wiki/WAV"/>
+    /// </summary>
+    /// <remarks>.wav</remarks>
+    WAV = Mix_MusicType.MUS_WAV,
+
+    /// <summary>
+    /// Module File: <see href="https://en.wikipedia.org/wiki/MOD_%28file_format%29"/>
+    /// </summary>
+    /// <remarks>.mod</remarks>
+    MOD = Mix_MusicType.MUS_MOD,
+
+    /// <summary>
+    /// Musical Instrument Digital Interface: <see href="https://en.wikipedia.org/wiki/MIDI"/>
+    /// </summary>
+    /// <remarks>.mid or .midi</remarks>
+    MIDI = Mix_MusicType.MUS_MID,
+
+    /// <summary>
+    /// Ogg: <see href="https://en.wikipedia.org/wiki/Ogg"/>
+    /// </summary>
+    /// <remarks>.ogg</remarks>
+    OGG = Mix_MusicType.MUS_OGG,
+
+    /// <summary>
+    /// MPEG-1 Audio Layer III or MPEG-2 Audio Layer III: <see href="https://en.wikipedia.org/wiki/MP3"/>
+    /// </summary>
+    /// <remarks>.mp3</remarks>
+    MP3 = Mix_MusicType.MUS_MP3,
+
+    /// <summary>
+    /// Free Lossless Audio Codec: <see href="https://en.wikipedia.org/wiki/FLAC"/>
+    /// </summary>
+    /// <remarks>.flac</remarks>
+    FLAC = Mix_MusicType.MUS_FLAC,
+
+    /// <summary>
+    /// OPUS: <see href="https://en.wikipedia.org/wiki/Opus_(audio_format)"/>
+    /// </summary>
+    /// <remarks>.opus</remarks>
+    OPUS = Mix_MusicType.MUS_OPUS,
+}
