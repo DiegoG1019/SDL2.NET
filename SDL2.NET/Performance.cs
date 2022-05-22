dbbe319a0062ec9f5a0d7ca1153c2a4c1e86034b
Diego García <dagarciam1014@gmail.com>
2022-05-22 00:26:30 -0400

    Create Performance.cs

---
+++ SDL2.NET/Performance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SDL2.Bindings.SDL;
+
+namespace SDL2.NET;
+public static class Performance
+{
+    public static ulong PerformanceCounter => SDL_GetPerformanceCounter();
+    public static ulong PerformanceFrequency => SDL_GetPerformanceFrequency();
+}
