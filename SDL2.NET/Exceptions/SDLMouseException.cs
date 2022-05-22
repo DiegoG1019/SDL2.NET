fc8688ecccfa94d6129473174181aca553d41c45
Diego García <dagarciam1014@gmail.com>
2022-05-21 22:10:33 -0400

    Create SDLMouseException.cs

---
+++ SDL2.NET/Exceptions/SDLMouseException.cs
@@ -0,0 +1,29 @@
+﻿using SDL2.Bindings;
+using System.Runtime.CompilerServices;
+
+namespace SDL2.NET.Exceptions;
+
+[Serializable]
+public class SDLMouseException : SDLException
+{
+    public SDLMouseException() { }
+    public SDLMouseException(string message) : base(message) { }
+    public SDLMouseException(string message, Exception inner) : base(message, inner) { }
+    protected SDLMouseException(
+      System.Runtime.Serialization.SerializationInfo info,
+      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ThrowIfLessThan(int value, int comparison)
+    {
+        if (value < comparison)
+            throw new SDLMouseException(SDL.SDL_GetError());
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ThrowIfEquals(int value, int comparison)
+    {
+        if (value == comparison)
+            throw new SDLMouseException(SDL.SDL_GetError());
+    }
+}
