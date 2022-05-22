9239852de69b0d3e0719dd1678808f1a5dc919fb
Diego García <dagarciam1014@gmail.com>
2022-05-21 22:15:00 -0400

    Create Clipboard.cs

---
+++ SDL2.NET/Clipboard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2.NET;
+
+/// <summary>
+/// Provides an interface into the system's clipboard
+/// </summary>
+public static class Clipboard
+{
+    /// <summary>
+    /// Represents an event fired by the system's clipboard
+    /// </summary>
+    public delegate void ClipboardEvent();
+
+    /// <summary>
+    /// Fired when the system clipboard obtains a new value;
+    /// </summary>
+    public static event ClipboardEvent? ClipboardChanged;
+
+    internal static void TriggerClipboardChanged()
+        => ClipboardChanged?.Invoke();
+}
