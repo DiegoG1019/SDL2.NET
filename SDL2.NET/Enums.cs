64289fd9a83f50f25a91528c5219172bdde6985c
Diego García <dagarciam1014@gmail.com>
2022-05-23 04:22:19 -0400

    Update Enums.cs

--- SDL2.NET/Enums.cs
+++ SDL2.NET/Enums.cs
@@ -9,10 +9,46 @@ using static SDL2.Bindings.SDL;
 
 namespace SDL2.NET;
 
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 public static class EnumExtensions
 {
     public static Keycode ToKeycode(this Scancode code)
         => (Keycode)((int)code | SDLK_SCANCODE_MASK);
+}
+
+public enum BackendType : uint
+{
+    OpenGL = SDL_WindowFlags.SDL_WINDOW_OPENGL,
+    Vulkan = SDL_WindowFlags.SDL_WINDOW_VULKAN,
+    Metal = SDL_WindowFlags.SDL_WINDOW_METAL
+}
+
+public enum WindowFlags : uint
+{
+    Fullscreen = 0x00000001,
+    Opengl = 0x00000002,
+    Shown = 0x00000004,
+    Hidden = 0x00000008,
+    Borderless = 0x00000010,
+    Resizable = 0x00000020,
+    Minimized = 0x00000040,
+    Maximized = 0x00000080,
+    MouseGrabbed = 0x00000100,
+    InputFocus = 0x00000200,
+    MouseFocus = 0x00000400,
+    FullscreenDesktop = Fullscreen | 0x00001000,
+    Foreign = 0x00000800,
+    AllowHighDPI = 0x00002000, 
+    MouseCapture = 0x00004000,
+    AlwaysOnTop = 0x00008000,
+    SkipTaskbar = 0x00010000,  
+    Utility = 0x00020000,   
+    Tooltip = 0x00040000,   
+    PopupMenu = 0x00080000,
+    KeyboardGrabbed = 0x00100000,  
+    Vulkan = 0x10000000,
+    Metal = 0x2000000,  
+    InputGrabbed = MouseGrabbed,
 }
 
 [Flags]
@@ -855,3 +891,4 @@ public enum KeyModifier
 
     Reserved = Scroll
 }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
