60c770d7e895e378c120a1627207dac4f492f6d1
Diego García <dagarciam1014@gmail.com>
2022-05-23 14:30:03 -0400

    Documented WindowRenderer

--- SDL2.NET/WindowRenderer.cs
+++ SDL2.NET/WindowRenderer.cs
@@ -2,12 +2,24 @@
 
 namespace SDL2.NET;
 
+/// <summary>
+/// Represents a Renderer that is attached to one specific <see cref="Window"/>
+/// </summary>
 public class WindowRenderer : Renderer
 {
+    /// <summary>
+    /// The Window this Renderer is attached to
+    /// </summary>
     public Window AttachedWindow { get; }
 
+    /// <summary>
+    /// Instantiates a new <see cref="WindowRenderer"/> with a given <see cref="Window"/>
+    /// </summary>
     public WindowRenderer(Window window, int index = -1) : this(window, RendererFlags.Accelerated | RendererFlags.PresentVSync, index) { }
 
+    /// <summary>
+    /// Instantiates a new <see cref="WindowRenderer"/> with a given <see cref="Window"/> and <see cref="RendererFlags"/>
+    /// </summary>
     public WindowRenderer(Window window, RendererFlags flags, int index = -1) : base(SDL.SDL_CreateRenderer(window._handle, index, (SDL.SDL_RendererFlags)flags))
     {
         AttachedWindow = window;
