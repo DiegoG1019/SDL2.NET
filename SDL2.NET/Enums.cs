26502df90cd896c9f2b167c7ea6c2d79dfc85f62
Diego García <dagarciam1014@gmail.com>
2022-05-23 14:29:25 -0400

    Update Enums.cs

--- SDL2.NET/Enums.cs
+++ SDL2.NET/Enums.cs
@@ -23,34 +23,6 @@ public enum MaximizedType : uint
     Minimized = SDL_WindowFlags.SDL_WINDOW_MINIMIZED
 }
 
-public enum WindowFlags : uint
-{
-    Fullscreen = 0x00000001,
-    Opengl = 0x00000002,
-    Shown = 0x00000004,
-    Hidden = 0x00000008,
-    Borderless = 0x00000010,
-    Resizable = 0x00000020,
-    Minimized = 0x00000040,
-    Maximized = 0x00000080,
-    MouseGrabbed = 0x00000100,
-    InputFocus = 0x00000200,
-    MouseFocus = 0x00000400,
-    FullscreenDesktop = Fullscreen | 0x00001000,
-    Foreign = 0x00000800,
-    AllowHighDPI = 0x00002000, 
-    MouseCapture = 0x00004000,
-    AlwaysOnTop = 0x00008000,
-    SkipTaskbar = 0x00010000,  
-    Utility = 0x00020000,   
-    Tooltip = 0x00040000,   
-    PopupMenu = 0x00080000,
-    KeyboardGrabbed = 0x00100000,  
-    Vulkan = 0x10000000,
-    Metal = 0x2000000,  
-    InputGrabbed = MouseGrabbed,
-}
-
 [Flags]
 public enum MouseButton : byte
 {
