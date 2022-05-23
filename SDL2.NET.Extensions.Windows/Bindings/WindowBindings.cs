60fc00fb606984c57ea558c1b7b0ce4d39ed2a1c
Diego García <dagarciam1014@gmail.com>
2022-05-23 18:26:44 -0400

    Create WindowBindings.cs

---
+++ SDL2.NET.Extensions.Windows/Bindings/WindowBindings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2.NET.Extensions.Windows.Bindings;
+
+[SupportedOSPlatform("Windows")]
+public static class WindowBindings
+{
+    static WindowBindings()
+    {
+        if (!OperatingSystem.IsWindows())
+            throw new PlatformNotSupportedException("This static class can only be used when running on Windows");
+    }
+
+    public const int GWL_EXSTYLE = -20;
+    public const int WS_EX_LAYERED = 0x80000;
+    public const int LWA_ALPHA = 0x2;
+    public const int LWA_COLORKEY = 0x1;
+
+    [DllImport("user32.dll")]
+    public static extern long GetWindowLong(IntPtr hWnd, long nIndex);
+    [DllImport("user32.dll")]
+    public static extern long SetWindowLong(IntPtr hWnd, long nIndex, long dwNewLong);
+    [DllImport("user32.dll")]
+    public static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
+}
