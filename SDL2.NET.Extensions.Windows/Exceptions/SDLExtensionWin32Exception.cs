c5dc96c79f7f2e8ee098e60380f03c0dc7efd5a2
Diego García <dagarciam1014@gmail.com>
2022-05-24 03:24:52 -0400

    Create SDLExtensionWin32Exception.cs

---
+++ SDL2.NET.Extensions.Windows/Exceptions/SDLExtensionWin32Exception.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SDL2.NET.Extensions.Windows.Exceptions;
+
+[Serializable]
+public class SDLExtensionWin32Exception : Exception
+{
+    public SDLExtensionWin32Exception() { }
+    public SDLExtensionWin32Exception(string message) : base(message) { }
+    public SDLExtensionWin32Exception(string message, Exception inner) : base(message, inner) { }
+    protected SDLExtensionWin32Exception(
+      System.Runtime.Serialization.SerializationInfo info,
+      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
