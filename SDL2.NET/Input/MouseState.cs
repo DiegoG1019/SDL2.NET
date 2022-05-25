0b08174b552fc6726e13b4090d652bfddc83c24d
Diego García <dagarciam1014@gmail.com>
2022-05-24 22:22:24 -0400

    Create MouseState.cs

---
+++ SDL2.NET/Input/MouseState.cs
@@ -0,0 +1,26 @@
+﻿namespace SDL2.NET.Input;
+
+/// <summary>
+/// A structure containing data about the mouse's state
+/// </summary>
+public struct MouseState
+{
+    /// <summary>
+    /// The location that the mouse pointer was at the time of this state being contsructed
+    /// </summary>
+    public Point Location { get; }
+
+    /// <summary>
+    /// A set of flags denoting the buttons that were pressed at the time of this state being constructed
+    /// </summary>
+    public MouseButton PressedButtons { get; }
+
+    /// <summary>
+    /// Constructs a <see cref="MouseState"/> object to hold information about the mouse's state at a given time
+    /// </summary>
+    public MouseState(MouseButton pressedButtons, Point location)
+    {
+        Location = location;
+        PressedButtons = pressedButtons;
+    }
+}
