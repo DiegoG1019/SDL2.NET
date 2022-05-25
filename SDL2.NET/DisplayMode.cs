3847339da71d398e65243c016df8dadf0969a550
Diego García <dagarciam1014@gmail.com>
2022-05-25 02:33:44 -0400

    Create DisplayMode.cs

---
+++ SDL2.NET/DisplayMode.cs
@@ -0,0 +1,49 @@
+﻿using static SDL2.Bindings.SDL;
+
+namespace SDL2.NET;
+
+/// <summary>
+/// Represents information on a Display's display mode
+/// </summary>
+public struct DisplayMode
+{
+    public DisplayMode(PixelFormat format, int width, int height, int refreshRate)
+    {
+        Format = format;
+        Width = width;
+        Height = height;
+        RefreshRate = refreshRate;
+    }
+
+    /// <summary>
+    /// The Pixel format used by the display
+    /// </summary>
+    public PixelFormat Format { get; }
+
+    /// <summary>
+    /// The Width of the display in screen coordinates
+    /// </summary>
+    public int Width { get; }
+
+    /// <summary>
+    /// The Height of the display in screen coordinates
+    /// </summary>
+    public int Height { get; }
+
+    /// <summary>
+    /// The refresh rate of the display in Hz, or 0 if unspecified
+    /// </summary>
+    public int RefreshRate { get; }
+
+    public static explicit operator DisplayMode(SDL_DisplayMode mode)
+        => new((PixelFormat)mode.format, mode.w, mode.h, mode.refresh_rate);
+
+    public static explicit operator SDL_DisplayMode(DisplayMode mode)
+        => new()
+        {
+            w = mode.Width,
+            h = mode.Height,
+            format = (uint)mode.Format,
+            refresh_rate = mode.RefreshRate
+        };
+}
