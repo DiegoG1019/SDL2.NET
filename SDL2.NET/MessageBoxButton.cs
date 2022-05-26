33b114c296ee93a9dbd6039ddd639dcee7f187b9
Diego García <dagarciam1014@gmail.com>
2022-05-26 03:41:27 -0400

    Added MessageBox types

---
+++ SDL2.NET/MessageBoxButton.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SDL2.Bindings.SDL;
+
+namespace SDL2.NET;
+
+/// <summary>
+/// Represents a button in a message box
+/// </summary>
+/// <remarks>Feel free to inherit this class to add custom tagging data and even custom methods if you wish. The button id is discarded from this class, and is instead set by its position in the array internally</remarks>
+public class MessageBoxButton
+{
+    /// <summary>
+    /// Creates a new instance of <see cref="MessageBoxButton"/>
+    /// </summary>
+    /// <param name="text"></param>
+    public MessageBoxButton(string text)
+    {
+        ArgumentNullException.ThrowIfNull(text);
+        this.text = text;
+    }
+
+    private string text;
+
+    /// <summary>
+    /// The text describing this button
+    /// </summary>
+    public string Text
+    {
+        get => text; 
+        set
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            text = value;
+        }
+    }
+
+    /// <summary>
+    /// The flags to be applied to this button
+    /// </summary>
+    public MessageBoxButtonFlags Flags { get; set; }
+
+    internal SDL_MessageBoxButtonData ToSDL(int id)
+    {
+        return new()
+        {
+            text = Text,
+            buttonid = id,
+            flags = (SDL_MessageBoxButtonFlags)Flags
+        };
+    }
+}
---
+++ SDL2.NET/MessageBoxScheme.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SDL2.Bindings.SDL;
+
+namespace SDL2.NET;
+
+/// <summary>
+/// Represents a color scheme for a Message Box
+/// </summary>
+public sealed class MessageBoxColorScheme
+{
+    /// <summary>
+    /// The background color of the Message box
+    /// </summary>
+    public RGBColor Background { get; set; }
+
+    /// <summary>
+    /// The text color of the Message box
+    /// </summary>
+    public RGBColor Text {get; set; }
+
+    /// <summary>
+    /// The color of a button's borders in the Message box
+    /// </summary>
+    public RGBColor ButtonBorder {get; set; }
+
+    /// <summary>
+    /// The color of a button's background in the Message box
+    /// </summary>
+    public RGBColor ButtonBackground {get; set; }
+
+    /// <summary>
+    /// The color of a selected button in the Message box
+    /// </summary>
+    public RGBColor ButtonSelected {get; set; }
+
+    internal SDL_MessageBoxColorScheme ToSDL()
+    {
+        return new()
+        {
+            colors = new SDL_MessageBoxColor[]
+            {
+                new(){ r = Background.Red, b = Background.Blue, g = Background.Green  },
+                new(){ r = Text.Red, b = Text.Blue, g = Text.Green  },
+                new(){ r = ButtonBorder.Red, b = ButtonBorder.Blue, g = ButtonBorder.Green  },
+                new(){ r = ButtonBackground.Red, b = ButtonBackground.Blue, g = ButtonBackground.Green  },
+                new(){ r = ButtonSelected.Red, b = ButtonSelected.Blue, g = ButtonSelected.Green  },
+            }
+        };
+    }
+}
