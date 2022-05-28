23aa97a6bd0b118aada09411949e3811b3119919
Diego García <dagarciam1014@gmail.com>
2022-05-27 23:42:47 -0400

    Implemented SDL Power

---
+++ SDL2.NET/Power.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SDL2.Bindings.SDL;
+
+namespace SDL2.NET;
+
+/// <summary>
+/// Represents a way to access the system's power status
+/// </summary>
+public static class Power
+{
+    /// <summary>
+    /// Get the current power supply details.
+    /// </summary>
+    public static PowerStatus PowerState => new((PowerState)SDL_GetPowerInfo(out int secs, out int perc), TimeSpan.FromSeconds(secs), perc);
+}
---
+++ SDL2.NET/PowerStatus.cs
@@ -0,0 +1,35 @@
+﻿namespace SDL2.NET;
+
+/// <summary>
+/// Represents Power Status of the system
+/// </summary>
+public struct PowerStatus
+{
+    internal PowerStatus(PowerState powerState, TimeSpan remaining, int charge)
+    {
+        PowerState = powerState;
+        Remaining = remaining;
+        Charge = charge;
+    }
+
+
+    /// <summary>
+    /// The current Power State
+    /// </summary>
+    public PowerState PowerState { get; }
+
+    /// <summary>
+    /// The remaining time of battery life
+    /// </summary>
+    public TimeSpan Remaining { get; }
+
+    /// <summary>
+    /// The charge left in the battery,
+    /// </summary>
+    public int Charge { get; }
+
+    /// <summary>
+    /// The percentage of charge left, from 0.0 to 1.0
+    /// </summary>
+    public float ChargePercent => Charge / 100;
+}
