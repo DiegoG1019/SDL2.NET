8a16ebc19f27498499e43baeab02dc0597233ee5
Diego García <dagarciam1014@gmail.com>
2022-05-27 23:43:55 -0400

    Create KeyState.cs

---
+++ SDL2.NET/Input/KeyState.cs
@@ -0,0 +1,23 @@
+﻿namespace SDL2.NET.Input;
+
+/// <summary>
+/// Represents the status of a given key
+/// </summary>
+public struct KeyState
+{
+    /// <summary>
+    /// The scancode of they key
+    /// </summary>
+    public Scancode Scancode { get; }
+
+    /// <summary>
+    /// Whether the key was pressed at the time of this snapshot or not
+    /// </summary>
+    public bool IsPressed { get; }
+
+    internal KeyState(Scancode scancode, bool isPressed)
+    {
+        Scancode = scancode;
+        IsPressed = isPressed;
+    }
+}
