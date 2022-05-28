7a95d7a5d4c922f1bd5249e6e31f30df27d91aa8
Diego García <dagarciam1014@gmail.com>
2022-05-28 02:55:03 -0400

    Create MessageBoxTests.cs

---
+++ SDL2.NET Tests/MessageBoxTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2.NET.Tests;
+
+[SDLTestRepository("Message Box tests")]
+internal class MessageBoxTests
+{
+    [SDLTestMethod("Test Window Box")]
+    public static void TestWindowBox(RunControl _)
+    {
+        var app = SDLApplication.Instance().LaunchWindow("Test Window Box", 100, 100);
+        app.MainWindow.ShowMessageBox("This is a test!", "Don't mind me!", MessageBoxFlags.Information);
+        app.MainWindow.ShowMessageBox("This is a test!", "Don't mind me!", MessageBoxFlags.Warning);
+        app.MainWindow.ShowMessageBox("This is a test!", "Don't mind me!", MessageBoxFlags.Error);
+        app.MainWindow.Dispose();
+    }
+
+    [SDLTestMethod("Test Window Box Style")]
+    public static void TestWindowBoxStyle(RunControl _)
+    {
+        var app = SDLApplication.Instance();
+        var buttons = new MessageBoxButton[3]
+        {
+            new("Button 1"),
+            new("Button 2"),
+            new("Button 3")
+        };
+        var style = new MessageBoxColorScheme()
+        {
+            Background = Colors.AliceBlue,
+            ButtonBackground = Colors.AntiqueWhite,
+            ButtonBorder = Colors.Gray,
+            ButtonSelected = Colors.IndianRed,
+            Text = Colors.LavenderBlush
+        };
+
+        app.ShowMessageBox("This is a test!", "Don't mind me!", MessageBoxFlags.Information, buttons, style);
+        app.ShowMessageBox("This is a test!", "Don't mind me!", MessageBoxFlags.Warning, buttons, style);
+        app.ShowMessageBox("This is a test!", "Don't mind me!", MessageBoxFlags.Error, buttons, style);
+    }
+}
