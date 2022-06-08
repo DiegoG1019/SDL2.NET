6a432c47a7ed280a4a45b1ffbac1829dbedf4fc3
Diego García <dagarciam1014@gmail.com>
2022-06-08 19:10:51 -0400

    Create SDLGameControllerException.cs

---
+++ SDL2.NET/Exceptions/SDLGameControllerException.cs
@@ -0,0 +1,29 @@
+﻿using SDL2.Bindings;
+using System.Runtime.CompilerServices;
+
+namespace SDL2.NET.Exceptions;
+
+[Serializable]
+public class SDLGameControllerException : SDLException
+{
+    public SDLGameControllerException() { }
+    public SDLGameControllerException(string message) : base(message) { }
+    public SDLGameControllerException(string message, Exception inner) : base(message, inner) { }
+    protected SDLGameControllerException(
+      System.Runtime.Serialization.SerializationInfo info,
+      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ThrowIfLessThan(int value, int comparison)
+    {
+        if (value < comparison)
+            throw new SDLGameControllerException(SDL.SDL_GetError());
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ThrowIfEquals(int value, int comparison)
+    {
+        if (value == comparison)
+            throw new SDLGameControllerException(SDL.SDL_GetError());
+    }
+}
