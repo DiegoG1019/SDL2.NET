6aa8782070c03e1141c65a7cd64e748157551cfd
Diego García <dagarciam1014@gmail.com>
2022-06-09 12:48:50 -0400

    Create IJoystickDefinition.cs

---
+++ SDL2.NET/IJoystickDefinition.cs
@@ -0,0 +1,29 @@
+﻿namespace SDL2.NET;
+
+/// <summary>
+/// Represents special data about a Joystick's form and function
+/// </summary>
+public interface IJoystickDefinition
+{
+    /// <summary>
+    /// Whether this Joystick is a Virtual one
+    /// </summary>
+    /// <param name="virtualJoystick"></param>
+    public bool IsVirtual(out VirtualJoystick? virtualJoystick);
+
+    /// <summary>
+    /// Whether this Joystick is actually a GameController
+    /// </summary>
+    /// <param name="gameController"></param>
+    public bool IsGameController(out GameController? gameController);
+
+    /// <summary>
+    /// Whether this Joystick is a Virtual one
+    /// </summary>
+    public bool IsVirtual();
+
+    /// <summary>
+    /// Whether this Joystick is actually a GameController
+    /// </summary>
+    public bool IsGameController();
+}
