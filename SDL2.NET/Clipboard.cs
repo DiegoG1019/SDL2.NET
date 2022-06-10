a0ac98472870a5d0509069a75be64caf5d508cdd
Diego García <dagarciam1014@gmail.com>
2022-06-10 17:24:10 -0400

    Update Clipboard.cs

--- SDL2.NET/Clipboard.cs
+++ SDL2.NET/Clipboard.cs
@@ -1,26 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SDL2.NET.Exceptions;
+using static SDL2.Bindings.SDL;
 
 namespace SDL2.NET;
 
 /// <summary>
 /// Provides an interface into the system's clipboard
 /// </summary>
+/// <remarks>
+/// SDL's Video Subsystem must be initialized before this class can be used. This class only supports raw text, image data or file data are not supported, due to SDL's limitations
+/// </remarks>
 public static class Clipboard
 {
     /// <summary>
     /// Represents an event fired by the system's clipboard
     /// </summary>
-    public delegate void ClipboardEvent();
+    public delegate void ClipboardEvent(string? newText);
 
     /// <summary>
     /// Fired when the system clipboard obtains a new value;
     /// </summary>
     public static event ClipboardEvent? ClipboardChanged;
 
+    /// <summary>
+    /// Access the current text the system clipboard has
+    /// </summary>
+    public static string? ClipboardText
+    {
+        get => SDLClipboardException.ThrowIfEmpty(SDL_HasClipboardText() == SDL_bool.SDL_TRUE ? SDL_GetClipboardText() : null);
+        set => SDLClipboardException.ThrowIfLessThan(SDL_SetClipboardText(value ?? ""), 0);
+    }
+
     internal static void TriggerClipboardChanged()
-        => ClipboardChanged?.Invoke();
+    {
+        ClipboardChanged?.Invoke(ClipboardText);
+    }
 }
