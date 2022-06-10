66285c657cf720096331aad85bd94619cdc42592
Diego García <dagarciam1014@gmail.com>
2022-06-10 17:24:13 -0400

    Create ClipboardTests.cs

---
+++ SDL2.NET Tests/ClipboardTests.cs
@@ -0,0 +1,41 @@
+﻿using SDL2.NET.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2.NET.Tests;
+
+[SDLTestRepository("Clipboard Tests")]
+internal class ClipboardTests
+{
+    [SDLTestMethod("Display clipboard text")]
+    public static void DisplayText(RunControl _)
+    {
+        var app = SDLApplication.Instance().InitializeVideo();
+
+        var buttons = new MessageBoxButton[2]
+        {
+            new MessageBoxButton("Update")
+            {
+                Flags = MessageBoxButtonFlags.ReturnKeyDefault
+            },
+            new MessageBoxButton("Exit")
+            {
+                Flags = MessageBoxButtonFlags.EscapeKeyDefault
+            }
+        };
+
+        while (_.IsRunning)
+        {
+            var text = Clipboard.ClipboardText;
+            var pressed = app.ShowMessageBox("ClipboardText", text ?? "!No clipboard text", MessageBoxFlags.Information, buttons);
+            if (pressed?.Text is "Exit" or null)
+            {
+                _.IsRunning = false;
+                break;
+            }
+        }
+    }
+}
