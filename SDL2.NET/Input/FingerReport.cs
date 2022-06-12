dcea9c4ffac698b6a6ee61e491050283fbbb0135
Diego García <dagarciam1014@gmail.com>
2022-06-11 22:46:30 -0400

    Created report structs

---
+++ SDL2.NET/Input/FingerReport.cs
@@ -0,0 +1,35 @@
+﻿namespace SDL2.NET.Input;
+
+/// <summary>
+/// Represents touch information
+/// </summary>
+public struct FingerReport
+{
+    public FingerReport(FingerId id, float x, float y, float pressure)
+    {
+        Id = id;
+        X = x;
+        Y = y;
+        Pressure = pressure;
+    }
+
+    /// <summary>
+    /// The finger id
+    /// </summary>
+    public FingerId Id { get; set; }
+
+    /// <summary>
+    /// The x-axis location of the touch event, normalized (0...1)
+    /// </summary>
+    public float X { get; set; }
+
+    /// <summary>
+    /// The y-axis location of the touch event, normalized (0...1)
+    /// </summary>
+    public float Y { get; set; }
+
+    /// <summary>
+    /// The quantity of pressure applied, normalized (0...1)
+    /// </summary>
+    public float Pressure { get; set; }
+}
---
+++ SDL2.NET/Input/TouchReport.cs
@@ -0,0 +1,60 @@
+﻿using SDL2.NET.Exceptions;
+using System.Runtime.InteropServices;
+using static SDL2.Bindings.SDL;
+
+namespace SDL2.NET.Input;
+
+/// <summary>
+/// Represents a Report on a touch input device
+/// </summary>
+public struct TouchReport
+{
+    internal TouchReport(TouchId id)
+    {
+        Id = id;
+        DeviceType = (TouchDeviceType)SDL_GetTouchDeviceType(id);
+
+        var x = SDL_GetNumTouchFingers(id);
+        SDLTouchException.ThrowIfLessThan(x, 1);
+        FingerCount = x;
+    }
+
+    /// <summary>
+    /// The identification of the Touch Device
+    /// </summary>
+    public TouchId Id { get; }
+
+    /// <summary>
+    /// The type of the device
+    /// </summary>
+    public TouchDeviceType DeviceType { get; }
+
+    /// <summary>
+    /// The currently active fingers on this device
+    /// </summary>
+    public int FingerCount { get; }
+
+    /// <summary>
+    /// Indexes the device to query for specific fingers
+    /// </summary>
+    /// <param name="index">The index of the finger to query</param>
+    /// <returns>A report on the given finger</returns>
+    public FingerReport this[int index]
+    {
+        get
+        {
+            IntPtr ptr;
+            if (index < 0 || index >= FingerCount || (ptr = SDL_GetTouchFinger(Id, index)) == IntPtr.Zero)
+                throw new IndexOutOfRangeException("The index is out of range");
+
+            var f = Marshal.PtrToStructure<SDL_Finger>(ptr);
+
+            return new FingerReport(
+                f.id,
+                f.x,
+                f.y,
+                f.pressure
+            );
+        }
+    }
+}
