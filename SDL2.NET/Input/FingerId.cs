1c1073df8f10115c01dd133ef12568f5bc7e7724
Diego García <dagarciam1014@gmail.com>
2022-06-11 22:46:18 -0400

    Created Id structs

---
+++ SDL2.NET/Input/FingerId.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SDL2.NET.Input;
+
+/// <summary>
+/// Represents the identification of a finger on a touch device
+/// </summary>
+/// <remarks>
+/// For all intents and purposes, this is a long value.
+/// </remarks>
+public readonly record struct FingerId(long Id)
+{
+    /// <inheritdoc/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override int GetHashCode() => Id.GetHashCode();
+
+    /// <inheritdoc/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override string? ToString() => Id.ToString();
+
+    /// <summary>
+    /// Converts a FingerId into its equivalent long value
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator long(FingerId touch) => touch.Id;
+
+    /// <summary>
+    /// Converts a long value into its equivalent FingerId
+    /// </summary>
+    /// <param name="touch"></param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator FingerId(long touch) => new(touch);
+}
---
+++ SDL2.NET/Input/TouchId.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SDL2.NET.Input;
+
+/// <summary>
+/// Represents the identification of a touch device
+/// </summary>
+/// <remarks>
+/// For all intents and purposes, this is a long value.
+/// </remarks>
+public readonly record struct TouchId(long Id)
+{
+    /// <inheritdoc/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override int GetHashCode() => Id.GetHashCode();
+
+    /// <inheritdoc/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override string? ToString() => Id.ToString();
+
+    /// <summary>
+    /// Converts a TouchId into its equivalent long value
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator long(TouchId touch) => touch.Id;
+
+    /// <summary>
+    /// Converts a long value into its equivalent TouchId
+    /// </summary>
+    /// <param name="touch"></param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator TouchId(long touch) => new(touch);
+}
