aad83a555df7b4dc76676b8d73cb5e9ef0fc9e3b
Diego García <dagarciam1014@gmail.com>
2022-06-13 17:07:39 -0400

    Create Haptic.cs

---
+++ SDL2.NET/Haptic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDL2.NET;
+
+/// <summary>
+/// Represents a Haptic Device in SDL, and statically provides access to SDL Haptic related facilities
+/// </summary>
+public class Haptic
+{
+}
