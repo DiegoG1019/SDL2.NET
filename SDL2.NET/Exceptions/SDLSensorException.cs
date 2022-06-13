f2442d44e92cb6eb03b39ab720d5f0bf7257680d
Diego García <dagarciam1014@gmail.com>
2022-06-13 16:47:45 -0400

    Create SDLSensorException.cs

---
+++ SDL2.NET/Exceptions/SDLSensorException.cs
@@ -0,0 +1,28 @@
+﻿using SDL2.Bindings;
+using System.Runtime.CompilerServices;
+
+namespace SDL2.NET.Exceptions;
+
+[Serializable]
+public class SDLSensorException : SDLException
+{
+    public SDLSensorException() { }
+    public SDLSensorException(string message) : base(message) { }
+    public SDLSensorException(string message, Exception inner) : base(message, inner) { }
+    protected SDLSensorException(
+      System.Runtime.Serialization.SerializationInfo info,
+      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int ThrowIfLessThan(int value, int comparison) 
+        => value < comparison ? throw new SDLSensorException(SDL.SDL_GetAndClearError()) : value;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int ThrowIfEquals(int value, int comparison) 
+        => value == comparison ? throw new SDLSensorException(SDL.SDL_GetAndClearError()) : value;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static T ThrowIfNull<T>(T value)
+        => value is null ? throw new SDLSensorException(SDL.SDL_GetAndClearError()) : value;
+}
+
