d420e051c142988c7acd05452aae8643af504e79
Diego Garcia <dagarciam1014@gmail.com>
2023-09-01 23:04:29 -0400

    Create SDLRWopCreationException.cs

---
+++ SDL2.NET/Exceptions/SDLRWopCreationException.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+using SDL2.Bindings;
+
+namespace SDL2.NET.Exceptions;
+
+[Serializable]
+public class SDLRWopCreationException : SDLException
+{
+    public SDLRWopCreationException() { }
+    public SDLRWopCreationException(string message) : base(message) { }
+    public SDLRWopCreationException(string message, Exception inner) : base(message, inner) { }
+    protected SDLRWopCreationException(
+      System.Runtime.Serialization.SerializationInfo info,
+      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ThrowIfLessThan(int value, int comparison)
+    {
+        if (value < comparison)
+            throw new SDLRWopCreationException(SDL.SDL_GetAndClearError());
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ThrowIfEquals(int value, int comparison)
+    {
+        if (value == comparison)
+            throw new SDLRWopCreationException(SDL.SDL_GetAndClearError());
+    }
+}
